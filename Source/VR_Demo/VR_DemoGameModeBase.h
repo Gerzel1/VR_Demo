16001777401010b53b3aadbd1cf4382c0bdea02f
Gerzel1 <brandongerzel@gmail.com>
2022-04-15 00:53:00 -0400

    added unreal files and updated git files

---
+++ Source/VR_Demo/VR_DemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VR_DemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VR_DEMO_API AVR_DemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
